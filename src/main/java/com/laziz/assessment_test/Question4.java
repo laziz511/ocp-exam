9372a0877ddfa5d212e3971ccfc03922ef49c669
l.djurayev <l.djurayev@uzum.com>
2024-12-17 10:27:56 +0500

    Question4 refined

--- src/main/java/com/laziz/assessment_test/Question4.java
+++ src/main/java/com/laziz/assessment_test/Question4.java
@@ -1,5 +1,6 @@
 package com.laziz.assessment_test;
 
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -9,18 +10,18 @@ import java.util.Collection;
  * Question: What is the output of this code?
  * Answer: D. The code does not terminate.
  * Description:
- * - aa
+ * - the source is an infinite stream
  */
 public class Question4 {
     public static void main(String[] args) {
         Predicate<String> empty = String::isEmpty;
         Predicate<String> notEmpty = empty.negate();
         var result = Stream.generate(() -> "")
-                .filter(notEmpty)
+                .filter(notEmpty) // nothing passed the filter
                 .collect(Collectors.groupingBy(k -> k))
                 .entrySet()
                 .stream()
-                .map(Entry::getValue)
+                .map(Map.Entry::getValue)
                 .flatMap(Collection::stream)
                 .collect(Collectors.partitioningBy(notEmpty));
         System.out.println(result);
