47f4c621c7b45da15ad746d883580c3e1507e5b5
l.djurayev <l.djurayev@uzum.com>
2024-12-24 11:23:39 +0500

    add chapter 4 theory part 6

---
+++ src/main/java/com/laziz/chapter4/part6/MultidimensionalArrays.java
@@ -0,0 +1,10 @@
+package com.laziz.chapter4.part6;
+
+public class MultidimensionalArrays {
+    public static void main(String[] args) {
+        int[][] vars1;              // 2D array
+        int vars2[][];              // 2D array
+        int[] vars3[];              // 2D array
+        int[] vars4[], space[][];   // a 2D AND a 3D array
+    }
+}
