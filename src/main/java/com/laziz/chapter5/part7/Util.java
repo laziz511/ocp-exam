296df2d683f3447349c29d986bc7fabcc703c62b
l.djurayev <l.djurayev@uzum.com>
2024-12-25 18:15:45 +0500

    add chapter 5 theory part 7

---
+++ src/main/java/com/laziz/chapter5/part7/Util.java
@@ -0,0 +1,9 @@
+package com.laziz.chapter5.part7;
+
+public class Util {
+    public static final String name = "this is test name";
+
+    public static String name() {
+        return name + "at method";
+    }
+}
